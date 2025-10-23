b10582155a281aea085e38ee636f92c44b5774f7
ALEXA KAHELY ZAPATA GALLEGOS <zS24016702@estudiantes.uv.mx>
2025-10-23 09:45:55 -0600

    Update BilleteraParque.java

--- Actividad_22_10/Parque/src/BilleteraParque.java
+++ Actividad_22_10/Parque/src/BilleteraParque.java
@@ -9,30 +9,54 @@ public BilleteraParque(int tickets, boolean festivo){
 
 
 }
+public BilleteraPrque(){
+    return this.tickets = 0;
+}
+public BilleteraPrque(int tickets){
+    return this.tickets = tickets;
+}
 public int getTickets(){
     return this.tickets;
 }
-
 public static getFestivo(){
     return this.festivo;
 }
 
 public void setTickets(int tickets){
-    this.tickets = tickets;
+   if(tickets>0){
+       return this.tickets;
+   }
 }
 
 public static setFestivo(boolean festivo){
-    this.festivo = festivo;
-
+  if(festivo = false){
+    return true;
+        }
+    return false;
 }
 
-public void agregarTickets(int){
-
+public void agregarTickets(int num){
+    if(tickets >= 0 && num > 0 ){
+     this.tickets += num;
+    }
 }
 
-public boolean removerTickets(int){
+public boolean removerTickets(int num){
+    if (tickets > 0 && tickets > num){
+     this.tickets -=num;
+    return true;
     
 }
 
+return false;
+
+}
+
+public String toString(){
+    return "tickets"+ tickets+
+           "festivo?"+ festivo?
+           "agregar tickets" +agregarTickets()+
+            "remover tickets" +removerTickets();
+}
 
 }
