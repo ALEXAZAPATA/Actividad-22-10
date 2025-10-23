9593110a6aeb4bc501adcdb8634d70e508e00d08
ALEXA KAHELY ZAPATA GALLEGOS <zS24016702@estudiantes.uv.mx>
2025-10-23 10:00:18 -0600

    Update App.java

--- Actividad_22_10/Parque/src/App.java
+++ Actividad_22_10/Parque/src/App.java
@@ -1,32 +1,36 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       int megatron = 0;
+        BilleteraParque Billeteara1= new BilleteraParque(100);
+       int opcion;
 
 
 do{
-    System.out.println("agregar tickets");
-    System.out.println("establecer tickets");
-    System.out.println("comprar premio");
-    System.out.println("establecer festivo");
-    System.out.println("salir");
+    System.out.println("1. agregar tickets");
+    System.out.println("2. establecer tickets");
+    System.out.println("3. comprar premio");
+    System.out.println("4. establecer festivo");
+    System.out.println("5. salir");
+    System.out.println("tu billetera tiene " +Billetera1.getTickets()+ "tickets);
 
 
 
-
-
-       switch (megatron) {
+       switch (opcion) {
     case 1:
-    System.out.println();
+    System.out.println("¿Cuántos boletos se agregan?");
+    Billletera1.agregarTickets();
         
         break;
 
     case 2:
-    System.out.println();
+    System.out.println("Establecer saldo de tickets en: ");
         break;
 
 
  case 3:
-    System.out.println();
+     System.out.println("1. Camiseta (150 tickets)");
+     System.out.println("2. Sombrero (350 tickets)");
+     System.out.println("3. Tenis (600 tickets)");
+     System.out.print("¿Qué premio desea comprar? ");
         break;
 
 
@@ -59,4 +63,5 @@ do{
 
 
     }
+
 
